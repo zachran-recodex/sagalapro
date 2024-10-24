d04d2b9cca81e15793061012b0d1359770e8ea70
Zachran Razendra <razezach15@gmail.com>
2024-10-24 17:33:28 +0700

    Create new user (Sylvia)

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -14,6 +14,7 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
+        // Daftar permission yang akan dibuat
         $permissions = [
             'manage services',
             'manage blogs',
@@ -25,35 +26,54 @@ class RolePermissionSeeder extends Seeder
             'manage settings',
         ];
 
-        foreach($permissions as $permission){
-            Permission::firstOrCreate(
-                [
-                    'name' => $permission
-                ]
-            );
+        // Membuat permission jika belum ada
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission,
+            ]);
         }
 
+        // Membuat role 'designer_manager'
         $designManagerRole = Role::firstOrCreate([
-            'name' => 'designer_manager'
+            'name' => 'designer_manager',
         ]);
 
+        // Daftar permission untuk role 'designer_manager'
         $designManagerPermissions = [
             'manage fleets',
             'manage services',
         ];
 
+        // Mengatur permission untuk role 'designer_manager'
         $designManagerRole->syncPermissions($designManagerPermissions);
 
+        // Membuat role 'super_admin'
         $superAdminRole = Role::firstOrCreate([
-            'name' => 'super_admin'
+            'name' => 'super_admin',
         ]);
 
-        $user = User::create([
-            'name' => 'Admin',
-            'email' => 'admin@sagalapro.com',
-            'password' => bcrypt('admin123')
-        ]);
+        // Membuat user Admin jika belum ada
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@sagalapro.com'],
+            [
+                'name' => 'Admin',
+                'password' => bcrypt('admin123'),
+            ]
+        );
 
-        $user->assignRole($superAdminRole);
+        // Assign role 'super_admin' ke Admin
+        $admin->assignRole($superAdminRole);
+
+        // Membuat user Sylvia jika belum ada
+        $sylvia = User::firstOrCreate(
+            ['email' => 'sylvia@sagalapro.com'],
+            [
+                'name' => 'Sylvia',
+                'password' => bcrypt('sylvia123'),
+            ]
+        );
+
+        // Assign role 'super_admin' ke Sylvia
+        $sylvia->assignRole($superAdminRole);
     }
 }
