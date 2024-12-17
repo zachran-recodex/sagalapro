08443e6b51faf857fdad9e9bd256c583cbad3cbb
Zachran Razendra <razezach15@gmail.com>
2024-12-17 15:55:34 +0700

    make foreach for widget

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,7 +12,50 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('admin.dashboard');
+        $widgets = [
+            [
+                'route' => route('admin.hero-sections.index'),
+                'icon' => 'tabler:carousel-horizontal',
+                'title' => 'Hero Sections',
+            ],
+            [
+                'route' => route('admin.services.index'),
+                'icon' => 'material-symbols:home-repair-service-outline',
+                'title' => 'Services',
+            ],
+            [
+                'route' => route('admin.fleets.index'),
+                'icon' => 'hugeicons:helicopter',
+                'title' => 'Fleets',
+            ],
+            [
+                'route' => route('admin.partners.index'),
+                'icon' => 'mdi:partnership-outline',
+                'title' => 'Partners',
+            ],
+            [
+                'route' => route('admin.blogs.index'),
+                'icon' => 'mdi:blog-outline',
+                'title' => 'Blogs',
+            ],
+            [
+                'route' => route('admin.faqs.index'),
+                'icon' => 'mdi:faq',
+                'title' => 'Faqs',
+            ],
+            [
+                'route' => route('admin.contacts.index'),
+                'icon' => 'material-symbols:contact-mail-outline',
+                'title' => 'Contacts',
+            ],
+            [
+                'route' => route('admin.settings.index'),
+                'icon' => 'icon-park-outline:setting-two',
+                'title' => 'Settings',
+            ],
+        ];
+
+        return view('admin.dashboard', compact('widgets'));
     }
 
     /**
